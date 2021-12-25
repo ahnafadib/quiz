aad51a4e960e148b9de5bb0a3df1e40f31de5246
Syed Ahnaf Adib Ahmed <43415862+ahnafadib@users.noreply.github.com>
2021-12-25 20:42:12 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Quiz} from './src/screens';
+const App = () => {
+  return (
+    <Quiz/>
+  );
+};
+
+export default App;
---
+++ src/screens/QuizData.js
@@ -0,0 +1,34 @@
+export default data = [
+    {
+        question: "Which falls under Basis Path Testing?",
+        options: ["System Testing","White Box Testing","Black Box Testing","Unit Testing"],
+        correct_option: "White Box Testing"
+    },
+
+    {
+        question: "What is the first step in the software development lifecycle?",
+        options: ["System Design","Preliminary Investigation and Analysis","System Testing","Coding"],
+        correct_option: "Preliminary Investigation and Analysis"
+    },
+    
+    {
+        question: "Attributes of good software is ____________.",
+        options: ["Development","Maintainability & functionality","Functionality"," Maintainability"],
+        correct_option: "Maintainability & functionality"
+    },
+    {
+        question: "Which is not suitable for accommodating any change?",
+        options: ["RAD Model","Waterfall Model","Prototyping Model","Extreme Programming"],
+        correct_option: "Waterfall Model"
+    },
+    {
+        question: "Quality Management is known as _______.",
+        options: ["SQI","SQA and SQM","SQA","SQM"],
+        correct_option: "SQA"
+    },
+    {
+        question: "Component level design is concerned with",
+        options: ["Flow oriented analysis","Class based analysis","Both of the above","None of the above"],
+        correct_option: "Both of the above"
+    }
+]
